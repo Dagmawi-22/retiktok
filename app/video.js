8f66b774755e189c7e00e2e7b6eddaf432480072
unknown <dagtt316@gmail.com>
2024-06-13 11:15:54 +0300

    refactor: make styles external

--- app/video.js
+++ app/video.js
@@ -13,6 +13,7 @@ import { Ionicons, AntDesign } from "@expo/vector-icons";
 import { useFocusEffect } from "@react-navigation/native";
 import * as VideoThumbnails from "expo-video-thumbnails";
 import { videos } from "../data/videos";
+import { styles } from "./styles/style";
 
 const screenHeight = Dimensions.get("window").height;
 const screenWidth = Dimensions.get("window").width;
@@ -202,76 +203,5 @@ const VideoScreen = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "black",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  fullScreen: {
-    width: "100%",
-    height: "100%",
-  },
-  landscapeWrapper: {
-    width: "100%",
-    height: "50%",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  video: {
-    width: "100%",
-    height: "100%",
-  },
-  landscapeVideo: {
-    width: "100%",
-    height: "50%",
-  },
-  thumbnailWrapper: {
-    position: "absolute",
-    top: "0%",
-    width: "100%",
-    height: "100%",
-  },
-  thumbnail: {
-    width: "100%",
-    opacity: 0.6,
-    height: screenHeight,
-  },
-  thumbnailOverlay: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  overlayVideo: {
-    width: "100%",
-    height: "50%",
-  },
-  buttonContainer: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  playButton: {
-    backgroundColor: "rgba(0, 0, 0, 0.5)",
-    borderRadius: 32,
-    padding: 8,
-  },
-  iconContainer: {
-    position: "absolute",
-    right: 10,
-    bottom: 100,
-    flexDirection: "column",
-    justifyContent: "space-between",
-  },
-  button: {
-    marginBottom: 20,
-  },
-});
 
 export default VideoScreen;
