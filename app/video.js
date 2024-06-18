371d4fa509a4dc1f4f552938c20e870bb3f31f35
unknown <dagtt316@gmail.com>
2024-06-18 14:32:53 +0300

    feat: fade in/out on swipe

--- app/video.js
+++ app/video.js
@@ -6,6 +6,7 @@ import {
   Image,
   TouchableOpacity,
   Dimensions,
+  Text,
 } from "react-native";
 import { Video } from "expo-av";
 import { Ionicons, AntDesign } from "@expo/vector-icons";
@@ -18,6 +19,10 @@ const VideoScreen = () => {
   const videoRef = useRef(null);
   const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
   const [thumbnail, setThumbnail] = useState(null);
+  const [videoDimensions, setVideoDimensions] = useState({
+    width: 0,
+    height: 0,
+  });
   const scaleValue = useRef(new Animated.Value(1)).current;
   const opacityValue = useRef(new Animated.Value(1)).current;
   const windowHeight = Dimensions.get("window").height;
@@ -26,17 +31,17 @@ const VideoScreen = () => {
     if (viewableItems.length > 0) {
       const index = viewableItems[0].index;
       if (index !== currentVideoIndex) {
-        // Start fade out animation
+        // fade out
         Animated.timing(opacityValue, {
-          toValue: 0,
-          duration: 300,
+          toValue: 0.5,
+          duration: 10,
           useNativeDriver: true,
         }).start(() => {
           setCurrentVideoIndex(index);
-          // Start fade in animation
+          // fade in
           Animated.timing(opacityValue, {
             toValue: 1,
-            duration: 300,
+            duration: 10,
             useNativeDriver: true,
           }).start();
         });
@@ -59,6 +64,17 @@ const VideoScreen = () => {
     }
   };
 
+  const handlePlaybackStatusUpdate = (status) => {
+    if (status.isLoaded && status.videoWidth && status.videoHeight) {
+      setVideoDimensions({
+        width: status.videoWidth,
+        height: status.videoHeight,
+      });
+    } else {
+      console.log("video status is", status);
+    }
+  };
+
   useFocusEffect(
     React.useCallback(() => {
       return () => {
@@ -71,16 +87,19 @@ const VideoScreen = () => {
 
   useEffect(() => {
     if (videoRef.current) {
-      videoRef.current.loadAsync(
-        { uri: videos[currentVideoIndex].uri },
-        { shouldPlay: true },
-        false
-      );
-    }
-    if (videos[currentVideoIndex].isLandscape) {
-      generateThumbnail(videos[currentVideoIndex].uri);
-    } else {
-      setThumbnail(null);
+      videoRef.current
+        .loadAsync(
+          { uri: videos[currentVideoIndex].uri },
+          { shouldPlay: true },
+          false
+        )
+        .then(() => {
+          if (videos[currentVideoIndex].isLandscape) {
+            generateThumbnail(videos[currentVideoIndex].uri);
+          } else {
+            setThumbnail(null);
+          }
+        });
     }
   }, [currentVideoIndex]);
 
@@ -101,6 +120,7 @@ const VideoScreen = () => {
         shouldPlay={true}
         isLooping
         style={item.isLandscape ? styles.landscapeVideo : styles.video}
+        onPlaybackStatusUpdate={handlePlaybackStatusUpdate}
       />
       {item.isLandscape && thumbnail && (
         <View style={styles.thumbnailWrapper}>
@@ -130,6 +150,11 @@ const VideoScreen = () => {
         </TouchableOpacity>
         <TouchableOpacity style={styles.button}>
           <Ionicons name="share-social" size={32} color="white" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={{ color: "#fff" }}>
+            Dimension {videoDimensions.width} x {videoDimensions.height}
+          </Text>
         </TouchableOpacity>
       </View>
     </Animated.View>
